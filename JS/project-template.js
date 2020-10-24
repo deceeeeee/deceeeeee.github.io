a359e1d250cf1d5ced097435282865a2f6a6dc17
deceeeeee <micdece69@gmail.com>
2020-10-25 02:24:37 +0700

    Never hide modal-content

--- JS/project-template.js
+++ JS/project-template.js
@@ -7,6 +7,6 @@ $(document).ready(function() {
 
   $(".modal").on("click", function() {
     $(".modal").fadeOut();
-    $(".modal .modal-content").hide();
+    // $(".modal .modal-content").hide();
   });
 });
