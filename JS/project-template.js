f4a46e7515869d247a7fd5347df8d7f5c8526088
deceeeeee <micdece69@gmail.com>
2020-10-27 13:31:29 +0700

    Add angular to all pages

--- JS/project-template.js
+++ JS/project-template.js
@@ -4,9 +4,4 @@ $(document).ready(function() {
   $("#content").on("click", function() {
     $(window).scrollTop(chevron);
   });
-
-  $(".modal").on("click", function() {
-    $(".modal").fadeOut();
-    // $(".modal .modal-content").hide();
-  });
 });
